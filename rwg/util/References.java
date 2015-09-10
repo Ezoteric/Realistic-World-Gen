6648a6f86df695f7197b96b71ff90928404d3493
Ezoteric <gemeenz@gmail.com>
2015-09-10 14:38:59 +0200

    Create References.java

---
+++ rwg/util/References.java
@@ -0,0 +1,7 @@
+package rwg.util;
+
+public class References {
+	public static final String MODID = "testmod2";
+
+	public static final String[] COLORS = new String[]{"black", "red", "green", "brown", "blue", "purple", "cyan", "silver", "gray", "pink", "lime", "yellow", "light_blue", "magenta", "orange", "white"};
+}
